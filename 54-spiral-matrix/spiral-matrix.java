3d2ebf6c815566fa291e15ac9087803ee13bf2a4
kwatramuskan <kwatramuskan2007@gmail.com>
2026-07-17 13:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (9.20%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -4,30 +4,36 @@ class Solution {
         int rows = matrix.length;
         int columns = matrix[0].length;
         int topRow = 0 ;
-        int bottomRow = matrix.length - 1 ; 
+        int bottomRow = matrix.length-1;
         int leftColumn = 0;
         int rightColumn = matrix[0].length-1;
-        int totalElements = 0;
+        int totalElements = 0 ;
         int total = rows*columns;
         while(totalElements<total){
-            for(int j = leftColumn ; j<=rightColumn && totalElements<total ; j++){
+            for(int j = leftColumn ; j<=rightColumn&&totalElements<total ; j++){
                 result.add(matrix[topRow][j]);
                 totalElements++;
+               
             }
-            topRow++;
-            for(int i = topRow ; i<=bottomRow  && totalElements<total ; i++){
+             topRow++;
+            for(int i =topRow ; i<=bottomRow &&totalElements<total; i++){
                 result.add(matrix[i][rightColumn]);
                 totalElements++;
+                
+
             }
             rightColumn--;
-            for(int j = rightColumn ; j>= leftColumn && totalElements<total ; j--){
+            for(int j = rightColumn ; j>=leftColumn &&totalElements<total ; j--){
                 result.add(matrix[bottomRow][j]);
                 totalElements++;
+               
+
             }
-            bottomRow--;
-            for(int i = bottomRow ; i>= topRow && totalElements<total ; i--){
+             bottomRow--;
+            for(int i = bottomRow ; i>=topRow &&totalElements<total; i--){
                 result.add(matrix[i][leftColumn]);
                 totalElements++;
+                
             }
             leftColumn++;
         }
