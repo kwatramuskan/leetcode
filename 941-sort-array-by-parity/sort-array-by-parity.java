e3624b6da787b3372def48ebbf4df707023a5582
kwatramuskan <kwatramuskan2007@gmail.com>
2026-07-23 17:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (30.94%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -3,12 +3,13 @@ class Solution {
         int left = 0 ;
         int right = nums.length-1;
         while(left<right){
-            if(nums[left]%2!=0&&nums[right]%2==0){
-                int temp = nums[left] ;
+            if(nums[left]%2!=0 && nums[right]%2==0){
+                int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
-
-            }else if ( nums[left]%2==0){
+                left++;
+                right--;
+            }else if(nums[left]%2==0){
                 left++;
             }else{
                 right--;
