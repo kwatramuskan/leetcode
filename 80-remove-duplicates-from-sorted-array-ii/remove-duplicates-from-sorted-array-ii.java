41f7387a19dd45fe7d663bde7468650272822701
kwatramuskan <kwatramuskan2007@gmail.com>
2026-07-23 16:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (50.88%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -3,15 +3,16 @@ class Solution {
         if(nums.length<=2){
             return nums.length;
         }
-        int x = 2;
-        for(int i = 2 ; i<nums.length ; i++) {
+        int x= 2;
+        for(int i = 2 ; i<nums.length ; i++){
             if(nums[i]!=nums[x-2]){
-                
                 nums[x] = nums[i];
                 x++;
 
             }
         }
         return x;
+
+          
     }
 }
