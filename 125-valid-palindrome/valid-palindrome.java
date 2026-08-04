b5750ecaaa77322e48bc7b2a2052f9e92239e30e
kwatramuskan <kwatramuskan2007@gmail.com>
2026-08-04 18:36:40 +0530

    Time: 2 ms (99.32%) | Memory: 44.2 MB (74.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0 ;
-        int right=s.length()-1;
+        int left =0;
+        int right = s.length()-1;
         while(left<right){
             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
@@ -9,14 +9,14 @@ class Solution {
             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-            //compare after converting 
             if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right))){
-              return false;
+                return false;
+                
             }
             left++;
-            right--;
-            
+              right--;
         }
-        return true ; 
-    }
+        return true;
+
+     }   
 }
