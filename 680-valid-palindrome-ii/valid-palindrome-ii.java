d02de5ade5b05ad6a2a76080f19253d7947daa24
kwatramuskan <kwatramuskan2007@gmail.com>
2026-08-04 18:55:51 +0530

    Time: 11 ms (6.59%) | Memory: 51.3 MB (12.14%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            if(s.charAt(left)==s.charAt(right)){
-               left++;
+       int left =0;
+       int right = s.length()-1;
+       while(left<right){
+        if(s.charAt(left)==s.charAt(right)){
+            left++;
             right--;
-            }else{
-                return check(s,left+1, right)||check(s,left,right-1);
-            }
-           
-            
+
+        }else{
+            return check(s,left+1,right) ||check( s,left,right-1);
         }
-        return true;
-         
-        
+       }
+       return true ;
     }
     public boolean check(String s , int left , int right){
-                 if(left>=right){
-                    return true ;
-                }
-                return  s.charAt(left)==s.charAt(right) && check (s , left+1 , right-1);
-            }
-}
+        if(left>=right){
+            return true;
+        }else{
+            return s.charAt(left)==s.charAt(right) && check(s,left+1 , right-1);
+        }
+    }
+} 
