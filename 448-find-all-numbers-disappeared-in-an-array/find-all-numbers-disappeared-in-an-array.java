feda25f1d0b853e755db87469e3d69eb4f9fc5bb
kwatramuskan <kwatramuskan2007@gmail.com>
2026-08-18 18:02:28 +0530

    Time: 24 ms (10.60%) | Memory: 71 MB (36.42%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Arrays.sort(nums);
-        ArrayList<Integer>result = new ArrayList<>();
-        int expected =1;
-        int i=0;
-        while(expected<=nums.length){
-              while (i < nums.length && nums[i] < expected) {
-                i++;
-            }
-            if(i<nums.length&&nums[i]==expected){
-                i++;//mil gya 
-            }else{
-                result.add(expected);
-            }
-            expected++;
+       Arrays.sort(nums);
+       ArrayList<Integer>result = new ArrayList<>();
+       int first = 0;
+       int second = 1;
+       while(second<=nums.length){
+        while(first<nums.length && nums[first]<second){
+            first++;
         }
-        return result;
-
+        if(first<nums.length && nums[first]==second){
+            first++;
+        }else{
+            result.add(second);
+        }
+        second++;
+       }
+       return result;
         
     }
 }
