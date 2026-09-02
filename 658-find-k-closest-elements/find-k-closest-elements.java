984f7018411248fe711f50c61b01473f7a1b6f8a
kwatramuskan <kwatramuskan2007@gmail.com>
2026-09-02 13:04:16 +0530

    Time: 4 ms (97.14%) | Memory: 48 MB (35.79%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0 ;
+        int right = arr.length-1;
+        while(right-left+1>k){
+            if(Math.abs(arr[left]-x)<= Math.abs(arr[right]-x)){
+                right--;
+
+            }else{
+                left++;
+            }
+        }
+        List<Integer>ans = new ArrayList<>();
+        for(int i =left ; i<=right ; i++){
+            ans.add(arr[i]);
+        }
+        return ans ;
+    }
+}
